6e9b0db48d95bf5033092abd872e8c725d59b6c8
Professor Taki <Taki21@users.noreply.github.com>
2021-11-29 20:55:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,4 +17,35 @@ function runFunction() {
     i++;
 }
 
+
+function showSRank() { 
+    document.getElementById("pug-photo").src = 'https://cdn.discordapp.com/attachments/912012302830694410/914894175160401950/The_Pug_Mafi.png';
+} 
+
+function showARank() { 
+    document.getElementById("pug-photo").src = 'https://cdn.discordapp.com/attachments/912012302830694410/914894174917103657/The_Pug_Mafi.png';
+} 
+
+function showBRank() { 
+    document.getElementById("pug-photo").src = 'https://cdn.discordapp.com/attachments/912012302830694410/914894174636109844/The_Pug_Mafi.png';
+} 
+
+function showCRank() { 
+    document.getElementById("pug-photo").src = 'https://cdn.discordapp.com/attachments/912012302830694410/914894173981769779/20684A90-CCCB-48EA-BC37-00846566B777.png';
+} 
+
+function showDRank() { 
+    document.getElementById("pug-photo").src = 'https://cdn.discordapp.com/attachments/912012302830694410/914894173696589896/A8E50004-31D1-4E22-9215-DD612E112D9F.png';
+} 
+
+function upNDown() {
+    document.getElementsByClassName("bsimg")[0].style.transform = "translate(0, "+ 10*(Math.sin(Date.now() * (1/500))) +"px)";
+}
+
+function doUpAndDown() {
+    setInterval(upNDown, 1);
+}
+
+doUpAndDown();
+
 loopImages();
